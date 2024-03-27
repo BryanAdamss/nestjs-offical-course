92ff913f85bf30c1efcc84ab3d93953c9a17ff1c
BryanAdamss <bryanadamss@foxmail.com>
2024-03-27 22:29:27 +0800

    chore: use service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -24,28 +24,35 @@ export class CoffeesController {
 
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action return all coffees.Limit ${limit},offsets ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    // return `This action return all coffees.Limit ${limit},offsets ${offset}`;
+
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `返回#[${id}] coffee`;
+    // return `返回#[${id}] coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
-    return body;
+    // return body;
+
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action update #${id} coffee`;
+    // return `This action update #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    // return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
