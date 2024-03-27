0cfb9fe39f7576607eb8409ba960517d3d3af784
BryanAdamss <bryanadamss@foxmail.com>
2024-03-27 21:38:10 +0800

    chore: use express response

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,13 +6,15 @@ import {
   Post,
   HttpCode,
   HttpStatus,
+  Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return '返回所有coffee';
+  findAll(@Res() res) {
+    /** 使用底层express响应对象，不推荐，失去框架上层抽象意义 */
+    res.status(200).send('返回所有coffee');
   }
 
   @Get(':id')
