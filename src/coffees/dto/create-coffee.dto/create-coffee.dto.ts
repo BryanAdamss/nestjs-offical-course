7105e38eac6cdc33f764e220e80f35e8c1299a3d
BryanAdamss <bryanadamss@foxmail.com>
2024-03-28 22:51:43 +0800

    chore: validation pipe

--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsString({ /** each 表示每个元素都是string*/ each: true })
   readonly flavors: string[];
 }
--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
@@ -1,5 +1,8 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from '../create-coffee.dto/create-coffee.dto';
+
+/**
+ * 使用PartialType将继承CreateCoffeeDto的属性和所有验证pipe
+ * 并将属性转为可选(通过动态添加@IsOptional()实现)
+ */
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
