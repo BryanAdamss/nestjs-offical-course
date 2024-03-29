998bd64853f6217769329950addfc34f1fcaf6f7
BryanAdamss <bryanadamss@foxmail.com>
2024-03-29 22:37:53 +0800

    chore: Validation Pipe transform

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -22,6 +22,8 @@ export class CoffeesService {
 
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
+
+    return createCoffeeDto;
   }
 
   update(id: string, updateCoffeeDto: any) {
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   /** 使用全局ValidationPipe */
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto中未声明的属性会被过滤掉
+      forbidNonWhitelisted: true, // 出现dto中未声明的属性时，抛400 BadRequest错误
+      transform: true, // 转换数据类型，可将query、paramas转换为声明的类型
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
