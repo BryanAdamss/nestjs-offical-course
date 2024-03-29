42b41ab4dc8c1a7f192e88cc103f56db1d156484
BryanAdamss <bryanadamss@foxmail.com>
2024-03-30 00:23:43 +0800

    chore: add coffee entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
 
 @Module({
+  imports: [
+    /** 让typeorm知道CoffeeModule子模块内有哪些实体 */
+    TypeOrmModule.forFeature([Coffee]),
+  ],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * 每个用@Entity装饰的类都代表一个SQL表（SQL table）
+ * typeorm配置synchronize，会自动在数据库中创建此表
+ * 表名为lowercase<ClassName>，此处为coffee
+ * 也可传入一个字符串给@Entity()来指定表名
+ */
+@Entity()
 export class Coffee {
+  /** 自增列 */
+  @PrimaryGeneratedColumn()
   id: number;
+
+  /** 指定列，必填，值不能为空 */
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  /** 标识floavors在数据中以json形式存储，并允许null */
+  @Column('json', { nullable: true })
   flavors: string[];
 }
