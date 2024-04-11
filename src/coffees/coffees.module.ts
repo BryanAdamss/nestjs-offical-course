2f383edc6250c625803f090c9289b44684c1d909
BryanAdamss <bryanadamss@foxmail.com>
2024-04-11 23:01:36 +0800

    chore: custom provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,6 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 
 @Module({
   imports: [
@@ -12,7 +17,20 @@ import { Event } from '../events/entities/event.entity';
     TypeOrmModule.forFeature([Coffee, Flavor, Event]),
   ],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS,
+      useValue: ['buddy brew', 'nescafe'],
+    },
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
