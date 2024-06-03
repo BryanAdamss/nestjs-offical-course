c43f30483ac3b12e1ba6637eb3f40dc2b883a3fd
BryanAdamss <bryanadamss@foxmail.com>
2024-06-03 22:59:12 +0800

    chore: middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    /** 对所有路由路径使用LoggingMiddleware */
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+
+    /** 限定路径 */
+    // consumer.apply(LoggingMiddleware).forRoutes('coffees');
+
+    /** 限定路径+方法 */
+    /*
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes({ path: 'coffees', method: RequestMethod.GET });
+    */
+  }
+}
